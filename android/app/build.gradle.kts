5f2cc4e2b85f6a810eb84315af81fae452a22394
Piyush Kumar Gupta <piyush.kr.work@gmail.com>
2025-03-31 02:27:39 +0530

    Added dependencies, Updated NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.piyush.kr.work.flutter_bloc_clean_code"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
